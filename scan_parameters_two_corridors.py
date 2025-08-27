26d87a07eaa158fd46b38ed3596456c01fc5e984
Aviv-Ratzon <78256805+Aviv-Ratzon@users.noreply.github.com>
2025-08-27 12:41:20 +0300

    cmd run for two corridors

--- scan_parameters_two_corridors.py
+++ scan_parameters_two_corridors.py
@@ -13,24 +13,36 @@ import pickle as pkl
 from sklearn.decomposition import PCA
 import numpy as np
 from torch.nn import MSELoss
+import argparse
+import ast
 
 from run_sim import *
 from utils import *
 
+# Helper function to parse string to python object (for lists, bools, etc.)
+def parse_value(val):
+    try:
+        # Try to parse as a literal (list, int, float, bool, None, etc.)
+        return ast.literal_eval(val)
+    except Exception:
+        # If fails, return as string
+        return val
 
-num_seeds = 10
-num_workers = 4
-gpu_ids = np.arange(8)
-use_gpu = True
-debug = False
-result_path = './results/sweep_results_linear/two_corridors_S_11_L_5'
-run_type = 'single_var' #'all_combs'
-modify_vars = {
-    # 'G': np.arange(.5,1.1,0.1),
-    'max_move': np.arange(0, 11),
-}
-base_params = {
+def get_arg_parser():
+    parser = argparse.ArgumentParser(description="Parameter sweep for two corridors experiment")
+    parser.add_argument('--num_seeds', type=int, default=10, help='Number of seeds')
+    parser.add_argument('--num_workers', type=int, default=4, help='Number of workers')
+    parser.add_argument('--gpu_ids', type=str, default='0,1,2,3,4,5,6,7', help='Comma separated list of GPU ids')
+    parser.add_argument('--use_gpu', type=lambda x: x.lower() == 'true', default=True, help='Use GPU (True/False)')
+    parser.add_argument('--debug', type=lambda x: x.lower() == 'true', default=False, help='Debug mode (True/False)')
+    parser.add_argument('--result_path', type=str, default='./results/sweep_results_linear/two_corridors_S_11_L_5', help='Path to save results')
+    parser.add_argument('--run_type', type=str, default='single_var', choices=['single_var', 'all_combs'], help='Type of run')
+    parser.add_argument('--modify_vars', type=str, default="{'max_move': np.arange(0, 11)}", help='Dictionary of variables to modify (as python dict string)')
+    parser.add_argument('--base_params', type=str, default=None, help='Base params as python dict string (overrides defaults)')
+    return parser
 
+# Default config values
+DEFAULT_BASE_PARAMS = {
     'linear_net': True,
     'G': 1,
     'sig_2_h': None,
@@ -39,12 +51,52 @@ base_params = {
     'hidden_size': 33,
     'num_epochs': 10000,
     'algo_name': 'SGD',
-
     'loss_fn': nn.CrossEntropyLoss(),
     'L': 5,
     'corridor_dim': 1,
     'max_move': 15,
 }
+
+def get_config_from_args():
+    parser = get_arg_parser()
+    args = parser.parse_args()
+
+    # Parse gpu_ids
+    gpu_ids = np.array([int(x) for x in args.gpu_ids.split(',') if x.strip() != ''])
+
+    # Parse modify_vars
+    try:
+        # Allow np.arange in modify_vars string
+        local_vars = {'np': np}
+        modify_vars = eval(args.modify_vars, {}, local_vars)
+    except Exception as e:
+        print(f"Error parsing modify_vars: {e}")
+        modify_vars = {'max_move': np.arange(0, 11)}
+
+    # Parse base_params
+    if args.base_params is not None:
+        try:
+            # Allow np.arange, nn.CrossEntropyLoss, etc.
+            local_vars = {'np': np, 'nn': nn}
+            base_params = eval(args.base_params, {}, local_vars)
+        except Exception as e:
+            print(f"Error parsing base_params: {e}")
+            base_params = DEFAULT_BASE_PARAMS.copy()
+    else:
+        base_params = DEFAULT_BASE_PARAMS.copy()
+
+    # Return all config variables
+    return {
+        'num_seeds': args.num_seeds,
+        'num_workers': args.num_workers,
+        'gpu_ids': gpu_ids,
+        'use_gpu': args.use_gpu,
+        'debug': args.debug,
+        'result_path': args.result_path,
+        'run_type': args.run_type,
+        'modify_vars': modify_vars,
+        'base_params': base_params,
+    }
 
 def run_scenario(config_data):
     C = Config()
@@ -140,6 +192,7 @@ def worker_function(args):
     num_runs = args[2]
     start_time = args[3]
     data_dict_l = args[4]
+    result_path = args[5]
     result = run_scenario(config_data)
     # with counter_lock:
     #     counter.value += 1
@@ -162,19 +215,16 @@ def worker_function(args):
                 # pkl.dump(data_dict_l_save, f)
 
 
-def get_args_list_single_var():
+def get_args_list_single_var(num_seeds, modify_vars, base_params, gpu_ids):
     args_list = []
     for var_name in modify_vars.keys():
         iter_items = list(itertools.product(np.arange(0, num_seeds), modify_vars[var_name]))
         for i, (seed, var_val) in enumerate(iter_items):
-            # lr = 0.1**(8/2) if var_name != 'L' else 0.1**(var_val/2)
             args_list.append([{**base_params, **{'seed': seed, 'gpu_id': gpu_ids[i % len(gpu_ids)]}, **{var_name: var_val}},
                               f'sweep_params_{var_name}'])
-            # args_list.append([{**base_params, **{'seed': seed, 'gpu_id': gpu_ids[i % len(gpu_ids)]}, **{var_name: var_val}},
-                            #   f'sweep_params_combo'])
     return args_list
 
-def get_args_list_all_combs():
+def get_args_list_all_combs(base_params, modify_vars, gpu_ids):
     args_list = []
     iter_dicts = get_all_key_combinations(modify_vars)
     for i, curr_dict in enumerate(iter_dicts):
@@ -183,6 +233,18 @@ def get_args_list_all_combs():
     return args_list
 
 if __name__ == "__main__":
+    # Parse config from command line
+    config = get_config_from_args()
+    num_seeds = config['num_seeds']
+    num_workers = config['num_workers']
+    gpu_ids = config['gpu_ids']
+    use_gpu = config['use_gpu']
+    debug = config['debug']
+    result_path = config['result_path']
+    run_type = config['run_type']
+    modify_vars = config['modify_vars']
+    base_params = config['base_params']
+
     print("Starting time:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     # Your main function code here
     start_time = time.time()
@@ -190,16 +252,16 @@ if __name__ == "__main__":
     if not os.path.isdir(result_path):
         os.makedirs(result_path)
     if run_type == 'single_var':
-        args_list = get_args_list_single_var()
+        args_list = get_args_list_single_var(num_seeds, modify_vars, base_params, gpu_ids)
     elif run_type == 'all_combs':
-        args_list = get_args_list_all_combs()
+        args_list = get_args_list_all_combs(base_params, modify_vars, gpu_ids)
     
     # Create a managed list that can be shared between processes
     manager = Manager()
     data_dict_l = manager.list()
     
     np.random.shuffle(args_list)
-    args_list = [a + [len(args_list), start_time, data_dict_l] for a in args_list]
+    args_list = [a + [len(args_list), start_time, data_dict_l, result_path] for a in args_list]
     # subprocess
     counter = mp.Value('i', 0)
     counter_lock = mp.Lock()
